64f1f25f6ee1ea5ec959574e8de37e371decb176
Jônatas Ribas <91293805+JonatasRibas@users.noreply.github.com>
2024-02-20 14:23:23 -0300

    commit

--- conta/Menu.java
+++ conta/Menu.java
@@ -16,5 +16,17 @@ public class Menu {
         conta.sacar(200.0);
 
         conta.imprimirInfo();
+
+        ContaCorrente contaCorrente = new ContaCorrente("123", 1000.0, 10.0);
+        System.out.println("Conta Corrente:");
+        System.out.println("Saldo inicial: " + contaCorrente.getSaldo());
+        contaCorrente.depositar(500.0);
+        System.out.println("Saldo após depósito: " + contaCorrente.getSaldo());
+
+        ContaPoupanca contaPoupanca = new ContaPoupanca("456", 2000.0, 0.05);
+        System.out.println("\nConta Poupança:");
+        System.out.println("Saldo inicial: " + contaPoupanca.getSaldo());
+        contaPoupanca.depositar(300.0);
+        System.out.println("Saldo após depósito: " + contaPoupanca.getSaldo());
     }
 }
