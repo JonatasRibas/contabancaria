df0e852f4ed7be4a22b7bac535142f6db587f9cd
Jônatas Ribas <91293805+JonatasRibas@users.noreply.github.com>
2024-02-20 14:14:13 -0300

    commit

--- conta/Menu.java
+++ conta/Menu.java
@@ -1,7 +1,20 @@
 package conta;
 
+import conta.model.Conta;
+
 public class Menu {
     public static void main(String[] args) {
 
+        Conta conta = new Conta("12345", 1000.0);
+
+        conta.imprimirInfo();
+
+        conta.depositar(500.0);
+
+        conta.imprimirInfo();
+
+        conta.sacar(200.0);
+        
+        conta.imprimirInfo();
     }
 }
---
+++ conta/model/Conta.java
@@ -0,0 +1,44 @@
+package conta.model;
+
+public class Conta {
+    private String numero;
+    private double saldo;
+
+    public Conta(String numero, double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public void imprimirInfo() {
+        System.out.println("Número da conta: " + numero);
+        System.out.println("Saldo: " + saldo);
+    }
+}
